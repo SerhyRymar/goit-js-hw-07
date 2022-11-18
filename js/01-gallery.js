12447914c5eb439484ce1abb3cd83ef1cd927fed
SerhyRymar <serhyrymar@gmail.com>
2022-11-18 20:59:32 +0200

    Done_task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,5 +17,26 @@ function craetePictureCardsMarkup(galleryItems) {
   </a>
 </div>`;
     }).join(' ');
+
 }
+
+// const galleryLink = document.querySelector(".gallery__link");
+
+// galleryLink.addEventListener('click', function (e) {
+//     e.preventDefault();
+// });
+
+// 2.Реалізація делегування на div.gallery і отримання url великого зображення.
+
+galleryContainer.addEventListener("click",  clickOnGalleryContainer);
+
+function clickOnGalleryContainer(evt) {
+    if (!evt.target.classList.contains("gallery__image")) {
+			return;
+		}
+    console.log(evt.target);
+}
+
+// 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.Використовуй CDN сервіс jsdelivr 
+// і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
 

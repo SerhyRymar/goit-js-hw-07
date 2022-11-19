b2bdc5f7b92824d7a514704afa4f683dadcc9f3d
SerhyRymar <serhyrymar@gmail.com>
2022-11-19 21:55:43 +0200

    Done_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,42 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
 // 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї
 const galleryContainer = document.querySelector(".gallery");
-const pictureGallery = craetePictureCardsMarkup(galleryItems);
-
-galleryContainer.insertAdjacentHTML("beforeend", pictureGallery);
 
 function craetePictureCardsMarkup(galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
-        return `<div class="gallery__item">
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
                 <a class="gallery__link" href="${original}">
                 <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
   </a>
 </div>`;
-    }).join(' ');
-
+		})
+		.join(" ");
 }
 
-// const galleryLink = document.querySelector(".gallery__link");
-
-// galleryLink.addEventListener('click', function (e) {
-//     e.preventDefault();
-// });
+const pictureGallery = craetePictureCardsMarkup(galleryItems);
+galleryContainer.innerHTML = pictureGallery;
 
 // 2.Реалізація делегування на div.gallery і отримання url великого зображення.
-
-galleryContainer.addEventListener("click",  clickOnGalleryContainer);
+galleryContainer.addEventListener("click", clickOnGalleryContainer);
 
 function clickOnGalleryContainer(evt) {
-    if (!evt.target.classList.contains("gallery__image")) {
-			return;
+	evt.preventDefault();
+
+	if (evt.target.nodeName !== "IMG") {
+		return;
+	}
+
+// 3.Відкриття модального вікна по кліку на елементі галереї.
+	const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+	instance.show();
+
+// 4.Закриття з клавіатури
+	galleryContainer.addEventListener("keydown", (e) => {
+		if (e.code === "Escape") {
+			instance.close();
 		}
-    console.log(evt.target);
+	});
 }
-
-// 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.Використовуй CDN сервіс jsdelivr 
-// і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
-
